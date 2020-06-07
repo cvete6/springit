e171103b9b293e942d102a2f2bd0a3bf3d191fba
Cvete Trajkovska <Cvete.Trajkovska@netcetera.com>
2020-06-08 00:03:47 +0200

    Initial commit

---
+++ src/test/java/Reddit/Clone/SpringitApplicationTests.java
@@ -0,0 +1,13 @@
+package Reddit.Clone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
